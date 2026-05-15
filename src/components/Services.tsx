8a5c463fbb7e3890f355875fea2a2fb4ac48eec2
Rahul Chettri <rahulchettri2020@gmail.com>
2026-05-15 09:58:07 +0530

    feat: add Services section with interactive image hover slider component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -20,7 +20,7 @@ import { HoverSlider,
     id: "slide-6",
     title: "UI UX design",
     imageUrl:
-      "https://images.unsplash.com/photo-1688733720228-4f7a18681c4f?q=80&w=2487&auto=format&fit=max&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1661589354357-f56ddf86a0b4?q=80&w=1170&auto=format&fit=max&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: "slide-3",
@@ -32,7 +32,7 @@ import { HoverSlider,
     id: "slide-4",
     title: "graphic design",
     imageUrl:
-      "https://images.unsplash.com/photo-1726066012698-bb7a3abce786?q=80&w=2487&auto=format&fit=max&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1661382011487-cd3d6b1d9dff?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ]
 
